351c605b953b210f366ddcee570f8c26d487d485
graham <graham@blend.com>
2018-12-06 01:45:52 -0800

    template

---
+++ 2018/template/template.js
@@ -0,0 +1,18 @@
+t
+const _ = require('lodash');
+const fs = require('fs');
+
+const IS_TEST = false;
+
+fs.readFile(IS_TEST ? 'test.txt' : 'in.txt', 'utf8', function(err, contents) {
+  let ans;
+
+  // let inputs = contents;
+
+  let input = contents.split('\n');
+  input.pop();
+
+  
+
+  console.log('ans: ', ans);
+});
