31066559202b93c476e8bb55f028a29e126e49ea
graham <graham@blend.com>
2018-12-11 22:19:20 -0800

    working part 1

---
+++ 2018/12/12.js
@@ -0,0 +1,80 @@
+const _ = require('lodash');
+const fs = require('fs');
+
+const INITIAL_STATE = '#.......##.###.#.#..##..##..#.#.###..###..##.#.#..##....#####..##.#.....########....#....##.#..##...';
+
+const transforms = 
+`..... => .
+#.... => .
+..### => .
+##..# => #
+.###. => #
+...## => .
+#.#.. => .
+..##. => .
+##.#. => #
+..#.. => .
+.#... => #
+##.## => .
+....# => .
+.#.#. => .
+#..#. => #
+#.### => .
+.##.# => #
+.#### => .
+.#..# => .
+####. => #
+#...# => #
+.#.## => #
+#..## => .
+..#.# => #
+#.##. => .
+###.. => .
+##### => #
+###.# => #
+...#. => #
+#.#.# => #
+.##.. => .
+##... => #`.split('\n');
+
+const ITERATIONS = 20;
+const PADDING = 50;
+
+const padding = _.times(PADDING, () => '.');
+
+let currentState = [...padding, ...INITIAL_STATE.split(''), ...padding];
+
+for (let i = 0; i < ITERATIONS; i++) {
+  currentState = currentState.map((pot, ind) => {
+    const piece = currentState.slice(ind - 2, ind + 3).join('');
+
+    if (_.size(piece < 5)) {
+      return '.';
+    }
+
+    const found = _.find(transforms, transform => {
+      const split = transform.split(' => ');
+      if (split[0] === piece) {
+        return true;
+      }
+    });
+
+    if (found) {
+      return found.split(' => ')[1];
+    }
+
+    return '.';
+  });
+}
+
+let ans = 0;
+
+const offset = PADDING;
+
+for (let pot = 0; pot < _.size(currentState); pot++) {
+  if (currentState[pot] === '#') {
+    ans += pot - offset;
+  }
+}
+
+console.log('ans: ', ans);
