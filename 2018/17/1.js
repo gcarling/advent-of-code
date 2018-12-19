ec14d24b99e4209f91c653803d5418c88b4c26fb
Graham Carling <gbcarling@gmail.com>
2018-12-19 15:51:20 -0500

    got test working

--- 2018/17/1.js
+++ 2018/17/1.js
@@ -2,17 +2,30 @@ const _ = require('lodash');
 const fs = require('fs');
 const util = require('../util');
 
-const IS_TEST = 1;
+const IS_TEST = 0;
 
-const WATER_LOC = 500;
+const WATER_LOC = 499;
 
 const mats = {
   SAND: '.',
   CLAY: '#',
-  WATER: '~',
+  WATER_STILL: '~',
+  WATER_FALLING: '|',
 };
 
-function printGridWindow(grid, offset) {
+function isWater(mat) {
+  return _.includes([mats.WATER_FALLING, mats.WATER_STILL], mat);
+}
+
+function isFlowing(mat) {
+  return _.includes([mats.WATER_FALLING, mats.SAND], mat);
+}
+
+function isSolid(mat) {
+  return _.includes([mats.CLAY, mats.WATER_STILL], mat);
+}
+
+function printGridWindow(grid, offset = 250) {
   const startX = WATER_LOC - offset;
   const endX = WATER_LOC + offset;
   for (let i = 0; i < _.size(grid); i++) {
@@ -21,6 +34,7 @@ function printGridWindow(grid, offset) {
     }
     process.stdout.write('\n');
   }
+  process.stdout.write('\n');
 }
 
 function getClayPoints(input) {
@@ -53,7 +67,7 @@ function getClayPoints(input) {
 function initGrid(clayPoints) {
   const maxY = _.max(_.maxBy(clayPoints, ({ y }) => _.max(y)).y);
 
-  const grid = _.times(maxY, () => _.times(1001, () => mats.SAND));
+  const grid = _.times(maxY, () => _.times(1000, () => mats.SAND));
 
   clayPoints.forEach(({ x, y }) => {
     x.forEach(col => {
@@ -66,6 +80,66 @@ function initGrid(clayPoints) {
   return grid;
 }
 
+function handleWaterFall(grid, waterStartX, waterStartY) {
+  let waterX = waterStartX;
+  let waterY = waterStartY;
+
+  // while we are falling 1) in the grid 2) through sand
+  while (waterY < _.size(grid)-1 && isFlowing(grid[waterY+1][waterX])) {
+    grid[waterY][waterX] = mats.WATER_FALLING;
+    waterY++;
+  }
+
+  if (waterY >= _.size(grid)) return;
+
+  // check if we have a full solid base
+  let solidBase = true;
+  let curX; 
+
+  function flowToHole(x, y) {
+    printGridWindow(grid);
+    grid[y][x] = mats.WATER_FALLING;
+
+    // we're at the end
+    if (!grid[y+1]) return true;
+
+    if (!isSolid(grid[y+1][x])) {
+      handleWaterFall(grid, x, y+1);
+      return true;
+    }
+    return false;
+  }
+  
+  // moving left: if ground isn't solid, we have to bail
+  for (curX = waterX; isFlowing(grid[waterY][curX]); curX--) {
+    if (flowToHole(curX, waterY)) {
+      solidBase = false;
+      break;
+    }
+  }
+  const minX = curX+1;
+  for (curX = waterX; isFlowing(grid[waterY][curX]); curX++) {
+    if (flowToHole(curX, waterY)) {
+      solidBase = false;
+      break;
+    }
+  }
+  const maxX = curX-1;
+
+  // if we don't have a solid base to work on, return here
+  if (!solidBase) return;
+
+  // since we have a solid base, we can fill this in with solid water
+  for (x = minX; x <= maxX; x++) {
+    grid[waterY][x] = mats.WATER_STILL;
+  }
+
+  printGridWindow(grid);
+
+  // now we start over
+  handleWaterFall(grid, waterStartX, waterStartY);
+}
+
 fs.readFile(IS_TEST ? 'test.txt' : 'in.txt', 'utf8', function(err, contents) {
   let ans;
 
@@ -76,7 +150,9 @@ fs.readFile(IS_TEST ? 'test.txt' : 'in.txt', 'utf8', function(err, contents) {
 
   const grid = initGrid(clayPoints);
 
-  printGridWindow(grid, 10);
+  handleWaterFall(grid, WATER_LOC, 0);
+
+  ans = _.sumBy(grid, row => _.sumBy(row, square => _.includes([mats.WATER_FALLING, mats.WATER_STILL], square) ? 1 : 0));
 
   console.log('ans: ', ans);
 });
