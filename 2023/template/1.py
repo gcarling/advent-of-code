91706a23d3c4f13899bf7e05d07c75168ebadeb7
Graham Carling <graham@Janessas-MacBook-Air.local>
2024-05-16 16:43:10 -0400

    update template

--- 2023/template/1.py
+++ 2023/template/1.py
@@ -1,11 +1,13 @@
-import sys
+from argparse import ArgumentParser
 
 def solution(file_name):
-    print(file_name)
     for line in open(file_name):
         print(line, end='')
 
 if __name__=='__main__':
     print("Running...")
-    print(sys.argv[1:])
-    solution("test.txt" if "test" in sys.argv[1] else "in.txt")
+    print('')
+    parser = ArgumentParser()
+    parser.add_argument("-t", "--test", dest="is_test", help="Run in test mode", default=False, action='store_true')
+    args = parser.parse_args()
+    solution("test.txt" if args.is_test else "in.txt")
